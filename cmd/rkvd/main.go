1e0148a4e06843b709479220b68f61ffb04adbfd
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-04-05 12:49:15 +0200

    rkvd/main.go: Update flags due to change in configuration

--- cmd/rkvd/main.go
+++ cmd/rkvd/main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 
@@ -25,12 +26,13 @@ import (
 func main() {
 	var (
 		id               = flag.Uint64("id", 0, "server ID")
-		cluster          = flag.String("cluster", ":9201", "comma separated cluster servers")
+		servers          = flag.String("servers", ":9201,:9202,:9203,:9204,:9205,:9206,:9207", "comma separated list of server addresses")
+		cluster          = flag.String("cluster", "1,2,3", "comma separated list of server ids to form cluster with, [1 >= id <= len(servers)]")
 		bench            = flag.Bool("quiet", false, "Silence log output")
 		recover          = flag.Bool("recover", false, "Recover from stable storage")
 		batch            = flag.Bool("batch", true, "enable batching")
 		electionTimeout  = flag.Duration("election", time.Second, "How long servers wait before starting an election")
-		heartbeatTimeout = flag.Duration("heartbeat", 50*time.Millisecond, "How often a heartbeat should be sent")
+		heartbeatTimeout = flag.Duration("heartbeat", 20*time.Millisecond, "How often a heartbeat should be sent")
 		maxAppendEntries = flag.Uint64("maxappend", 10000, "Max entries per AppendEntries message")
 	)
 
@@ -43,10 +45,44 @@ func main() {
 		os.Exit(1)
 	}
 
-	nodes := strings.Split(*cluster, ",")
+	nodes := strings.Split(*servers, ",")
 
 	if len(nodes) == 0 {
+		fmt.Print("-server argument is required\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	selected := strings.Split(*cluster, ",")
+
+	var ids []uint64
+
+	for _, sid := range selected {
+		id, err := strconv.ParseUint(sid, 10, 64)
+
+		if err != nil {
+			fmt.Print("could not parse -cluster argument\n\n")
+			flag.Usage()
+			os.Exit(1)
+		}
+
+		if id <= 0 || id > uint64(len(nodes)) {
+			fmt.Print("invalid -cluster argument\n\n")
+			flag.Usage()
+			os.Exit(1)
+		}
+
+		ids = append(ids, id)
+	}
+
+	if len(ids) == 0 {
 		fmt.Print("-cluster argument is required\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if len(ids) > len(nodes) {
+		fmt.Print("-cluster specifies too many servers\n\n")
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -92,7 +128,8 @@ func main() {
 
 	node := raftgorums.NewNode(grpcServer, NewStore(), &raftgorums.Config{
 		ID:               *id,
-		Nodes:            nodes,
+		Servers:          nodes,
+		InitialCluster:   ids,
 		Batch:            *batch,
 		Storage:          storage,
 		ElectionTimeout:  *electionTimeout,
