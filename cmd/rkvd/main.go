e67ec62ee8549b25357902d6c2c9819b78ab780a
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-05-02 20:50:50 +0200

    rkvd/main.go: Fix file permissions

--- cmd/rkvd/main.go
+++ cmd/rkvd/main.go
@@ -121,7 +121,7 @@ func main() {
 
 	logFile, err := os.OpenFile(
 		fmt.Sprintf("%s%sraft%.2d.log", os.TempDir(), string(filepath.Separator), *id),
-		os.O_CREATE|os.O_TRUNC|os.O_APPEND|os.O_WRONLY, 0666,
+		os.O_CREATE|os.O_TRUNC|os.O_APPEND|os.O_WRONLY, 0600,
 	)
 
 	if err != nil {
