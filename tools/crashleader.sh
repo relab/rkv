ccf2f2e8b04121339ffbd5bb5d95cf0e366d5995
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-05-17 21:16:06 +0200

    tools: Fix for loop and add missing extra variable

--- tools/crashleader.sh
+++ tools/crashleader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for n in {01..04}; do
-    for T in {gorums,etcd,hashicorp}; do
-        ansible-playbook -v -i hosts crashleader.yml -e "backend=etcd throughput=50 output=cl_${n} maxentries=1024 clients=200";
+    for backend in {gorums,etcd,hashicorp}; do
+        ansible-playbook -v -i hosts crashleader.yml -e "backend=${backend} throughput=50 output=cl_${n} maxentries=1024 clients=200 time=60s";
     done;
 done;
