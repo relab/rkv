38255de2f23d30cac70fa2a6d68a487cbc2628d2
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-05-18 21:46:09 +0200

    scripts: Fix time variable not set

--- scripts/latencyvsthroughput.sh
+++ scripts/latencyvsthroughput.sh
@@ -2,36 +2,36 @@
 
 for n in {01..04}; do
     for T in {005,025,050,060,070,080,090,100,110,120,130} ; do
-        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=gorums throughput=$T output=${n} maxentries=1024 clients=200";
+        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=gorums throughput=$T output=${n} maxentries=1024 clients=200 time=60s";
     done;
 done;
 
 for n in {01..04}; do
     for T in {005,010,020,030,040,050,060,070,080}; do
-        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=gorums throughput=$T output=${n} maxentries=64 clients=200";
+        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=gorums throughput=$T output=${n} maxentries=64 clients=200 time=60s";
     done;
 done;
 
 for n in {01..04}; do
     for T in {005,025,050,060,070,080,090,100,110,120} ; do
-        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=hashicorp throughput=$T output=${n} maxentries=1024 clients=200";
+        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=hashicorp throughput=$T output=${n} maxentries=1024 clients=200 time=60s";
     done;
 done;
 
 for n in {01..04}; do
     for T in {005,010,020,030,040,050,060,070,080}; do
-        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=hashicorp throughput=$T output=${n} maxentries=64 clients=200";
+        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=hashicorp throughput=$T output=${n} maxentries=64 clients=200 time=60s";
     done;
 done;
 
 for n in {01..04}; do
     for T in {005,025,050,060,070,080,090,100,110,120} ; do
-        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=etcd throughput=$T output=${n} maxentries=1024 clients=200";
+        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=etcd throughput=$T output=${n} maxentries=1024 clients=200 time=60s";
     done;
 done;
 
 for n in {01..04}; do
     for T in {005,010,020,030,040,050,060,070,080}; do
-        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=etcd throughput=$T output=${n} maxentries=64 clients=200";
+        ansible-playbook -v -i hosts latencyvsthroughput.yml -e "backend=etcd throughput=$T output=${n} maxentries=64 clients=200 time=60s";
     done;
 done;
--- scripts/partitionfollower.sh
+++ scripts/partitionfollower.sh
@@ -2,6 +2,6 @@
 
 for n in {01..04}; do
     for backend in {gorums,etcd,hashicorp}; do
-        ansible-playbook -v -i hosts partitionfollower.yml -e "backend=${backend} throughput=50 output=pf_${n} maxentries=1024 clients=200";
+        ansible-playbook -v -i hosts partitionfollower.yml -e "backend=${backend} throughput=50 output=pf_${n} maxentries=1024 clients=200 time=60s";
     done;
 done;
