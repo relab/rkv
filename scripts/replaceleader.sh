70edf73ae7ac2ece85700fa0ef0f52d418b97983
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-05-20 16:55:39 +0200

    scripts/replaceleader.sh: Run for all backends

--- scripts/replaceleader.sh
+++ scripts/replaceleader.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 for n in {01..04}; do
-    ansible-playbook -v -i hosts replaceleader.yml -e "backend=gorums throughput=50 output=rl_${n} maxentries=1024 clients=200 time=60s";
+    for backend in {gorums,etcd,hashicorp}; do
+        ansible-playbook -v -i hosts replaceleader.yml -e "backend=${backend} throughput=20 output=rl_${n} maxentries=1024 clients=200 time=60s";
+    done;
 done;
