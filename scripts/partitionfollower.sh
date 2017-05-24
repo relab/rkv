17e5f95ecc94024673fa2c61a8a95086e5113f97
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-05-24 18:54:22 +0200

    scripts: Run experiments with and without ordering

--- scripts/partitionfollower.sh
+++ scripts/partitionfollower.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-for n in {01..04}; do
-    for backend in {gorums,etcd,hashicorp}; do
-        ansible-playbook -v -i hosts partitionfollower.yml -e "backend=${backend} throughput=50 output=pf_${n} maxentries=1024 clients=200 time=60s";
+for n in {01..05}; do
+    for T in 50; do
+        for backend in gorums; do
+            for order in {true,false}; do
+                ansible-playbook -v -f 10 -i hosts partitionfollower.yml -e "backend=${backend} throughput=${T} output=pf_${n} maxentries=1024 clients=200 time=80s order=${order}";
+            done;
+        done;
+        for backend in {hashicorp,etcd}; do
+            ansible-playbook -v -f 10 -i hosts partitionfollower.yml -e "backend=${backend} throughput=${T} output=pf_${n} maxentries=1024 clients=200 time=80s order=true";
+        done;
     done;
 done;
